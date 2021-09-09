e797f015c5181b928f3ad84cf77dd0dd7e95ec69
robin nordin <robin.nordin2@gmail.com>
2021-09-09 10:03:53 +0200

    initial commit
    Co-Authored-By: Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>

---
+++ webshop-unittest/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Shop />
+    <Checkout />
+    <Shoppingcart />
+  </div>
+</template>
+
+<script>
+import Shop from './views/Shop.vue'
+import Checkout from './views/Checkout.vue'
+import Shoppingcart from './components/Shoppingcart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Shoppingcart,
+    Checkout,
+    Shop
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ webshop-unittest/src/views/Shop.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <li>{{item.titel}}</li>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item: {
+        titel: "",
+        price: "",
+        description: "",
+      },
+    };
+  },
+
+  methods: {
+   async getItems() {
+      const response = await fetch("https://fakestoreapi.com/products/1")
+      const data = await response.json();
+      this.item.titel = data.titel; 
+      this.item.price = data.price;
+      this.item.description = data.description; 
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>

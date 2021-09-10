c5c240b819277fbd01615ddb13c78b4bc656e3b1
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2021-09-10 09:37:35 +0200

    fetch peoducts

--- webshop-unittest/src/views/Shop.vue
+++ webshop-unittest/src/views/Shop.vue
@@ -1,33 +1,69 @@
 <template>
   <main>
-    <li>{{item.titel}}</li>
+    <div v-if="products">
+      <p>{{ products.title }}</p>
+    </div>
   </main>
 </template>
 
 <script>
 export default {
+
   data() {
     return {
-      item: {
-        titel: "",
-        price: "",
-        description: "",
-      },
-    };
+      products: {
+        id: '',
+        title: '',
+      }
+    }
+  },
+
+  created() {
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => (this.products.id = data.id) + (this.products.title = data.title))
+      .then(json => console.log(json));
   },
 
   methods: {
+    /**mounted() {
+    fetch('https://fakestoreapi.com/products' + this.id)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      //.then(data => this.products = data)
+  }, */
+    /*methods: {
    async getItems() {
-      const response = await fetch("https://fakestoreapi.com/products/1")
+      const response = await fetch("https://fakestoreapi.com")
       const data = await response.json();
-      this.item.titel = data.titel; 
+      this.item.title = data.title; 
       this.item.price = data.price;
-      this.item.description = data.description; 
+      this.item.description = data.description;
     },
   },
-}
+     async getFakes () {
+     fetch('https://fakestoreapi.com/products' + this.id)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      //.then(data => this.products = data)
+   },
+
+   async getItems() {
+      const response = await fetch("https://fakestoreapi.com")
+      const data = await response.json();
+      this.item.title = data.title; 
+      this.item.price = data.price;
+      this.item.description = data.description;
+    },
+
+   mounted: function () {
+     this.$nextTick(function () {
+       this.getFakes
+     })
+   },
+  */
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

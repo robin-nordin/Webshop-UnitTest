e778a63aa91606d05d702ef66db113b9fc50b18b
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2021-09-15 12:38:42 +0200

    update css shop

--- webshop-unittest/src/App.vue
+++ webshop-unittest/src/App.vue
@@ -22,10 +22,11 @@ export default {
   box-sizing: border-box;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Courier New", Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: darkgray;
 }
 </style>

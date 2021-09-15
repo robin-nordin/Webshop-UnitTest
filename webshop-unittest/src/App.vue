68264f9fbad00608fd0e0c67315d45b9e0c1987d
robin nordin <robin.nordin2@gmail.com>
2021-09-15 12:45:07 +0200

    Header component
    Created header component and changed some css

--- webshop-unittest/src/App.vue
+++ webshop-unittest/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <Shop />
   </div>
 </template>
 
 <script>
 import Shop from "./views/Shop.vue";
+import Header from "./components/Header.vue"
 
 export default {
   name: "App",
   components: {
+    Header,
     Shop,
   },
 };
@@ -19,7 +22,7 @@ export default {
 * {
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
+  box-sizing: border-box; 
 }
 #app {
   font-family: "Courier New", Courier, monospace;
